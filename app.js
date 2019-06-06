5f8d80d65f933a97aaf0ccb0d972a1a433f49554
seo9204 <seo9204@gmail.com>
2019-06-06 16:15:08 -0400

    fix app.js error to build react bundle

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
