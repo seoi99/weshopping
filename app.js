2cf524a4d6956e51120887c519fd10cb1e90ea1b
seo9204 <seo9204@gmail.com>
2019-06-16 23:50:10 -0400

    testing web scraping

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const $ = require('cheerio');
+const puppeteer = require('puppeteer');
+const axios = require('axios');
 
-// replace the uri string with your connection string.
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -33,6 +35,39 @@ if (process.env.NODE_ENV === 'production') {
 app.use('/admin', adminRouter);
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
+
+app.use('/scrap_puppeteer', (req, res) => {
+  const url = 'https://www.google.com/aclk?sa=l&ai=DChcSEwiI_eHx0cbiAhVQPmAKHWJ6BRUYABAgGgJ0bQ&sig=AOD64_04N7AnBs1G9jGS-TFKeRSnsRi9DA&ctype=5&q=&ved=0ahUKEwix-t3x0cbiAhUIW7wKHeOSBiEQ2CkI6wY&adurl=';
+
+  puppeteer
+    .launch()
+    .then(browser => browser.newPage())
+    .then(page => page.goto(url).then(() => page.content()))
+    .then((html) => {
+      $('img.primary-image', html).each(function read() {
+        console.log($(this));
+        console.log($(this)[0].attribs.src);
+      });
+    })
+    .catch((err) => {
+      // handle error
+    });
+});
+
+app.use('/scrap_axios', (req, res) => {
+  const url = 'https://www.google.com/aclk?sa=l&ai=DChcSEwiqy5769ojiAhVX5ZoKHU1_BzoYABApGgJsbQ&sig=AOD64_19gy1IwCiCGrMXHiXARmMwC2KucA&ctype=5&q=&ved=0ahUKEwi95Zn69ojiAhXHa1AKHV6kD7UQ2CkIgQY&adurl=';
+
+  axios.get(url)
+    .then((html) => {
+      $('img.primary-image', html).each(function () {
+        console.log($(this));
+        console.log($(this)[0].attribs.src);
+      });
+    })
+    .catch((err) => {
+      // handle error
+    });
+});
 
 app.listen(port, () => {
   debug(`listening at server ${chalk.green(port)}`);
